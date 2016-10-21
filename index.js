5c2f79f7332c6b4089845870b716404f392fe753
Tri Nguyen <tri@tridnguyen.com>
2016-10-21 15:11:47 -0400

    supports cors for main service

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express();
 var auth = require('./lib/auth');
 
-app.use('/', auth());
+app.use('/', auth({
+	cors: true
+}));
 app.listen(process.env.PORT || 3000, function () {
 	console.log('Express is listening.');
 });
