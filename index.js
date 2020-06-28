2117613218762a793be164b92a3958a2e0984425
Tri Nguyen <tri@tridnguyen.com>
2020-06-28 16:38:42 -0400

    Add methods to store and retrieve session from localstorage

---
+++ index.js
@@ -0,0 +1,52 @@
+/* global localStorage */
+import jwtDecode from 'jwt-decode';
+import pick from 'lodash.pick';
+
+// claims that are relevant
+const userClaims = [
+	'given_name',
+	'family_name',
+	'nickname',
+	'name',
+	'picture',
+	'sub'
+];
+// decode user info from idToken
+function getUser(idToken) {
+	const payload = jwtDecode(idToken);
+	return pick(payload, userClaims);
+}
+
+function isAuthenticated(user) {
+	return Date.now() < user.expiresAt;
+}
+
+export function storeSession(user) {
+	localStorage.setItem('access_token', user.accessToken);
+	localStorage.setItem('id_token', user.idToken);
+	localStorage.setItem('expires_at', JSON.stringify(user.expiresAt));
+}
+
+export function deleteSession() {
+	localStorage.removeItem('access_token');
+	localStorage.removeItem('id_token');
+	localStorage.removeItem('expires_at');
+}
+
+export function getSession() {
+	const expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+	if (isAuthenticated({ expiresAt })) {
+		// Auth0 example does not retrieve these tokens
+		// but it seems like they should be
+		const accessToken = localStorage.getItem('access_token');
+		const idToken = localStorage.getItem('id_token');
+		return {
+			expiresAt,
+			accessToken,
+			idToken,
+			profile: getUser(idToken)
+		};
+	}
+	// if not authenticated, remove any existing session
+	deleteSession();
+}
