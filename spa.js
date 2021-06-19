6480197c79f527576f522ba478940efc8b0521d3
Tri Nguyen <tri@tridnguyen.com>
2021-06-19 10:30:29 -0400

    fix async keyword

--- spa.js
+++ spa.js
@@ -1,6 +1,6 @@
 import createAuth0Client from 'auth0/auth0-spa-js';
 
-export function createAuth(options) {
+export async function createAuth(options) {
   if (!options.clientId) {
     throw new Error('clientId is required');
   }
