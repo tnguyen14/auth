1bdeff3e85c2546a875d9f66f4b1787711c2a556
Tri Nguyen <tri@tridnguyen.com>
2021-06-25 20:27:40 -0400

    Reuse token

--- server.js
+++ server.js
@@ -1,21 +1,63 @@
-import simpleFetch from 'simple-fetch';
+import auth0 from 'auth0';
+import jwt from 'jsonwebtoken';
+import jwksClient from 'jwks-rsa';
 
-const { postJson } = simpleFetch;
+import debug from 'debug';
+
+const log = debug('@tridnguyen/auth');
+
+const { AuthenticationClient } = auth0;
+
+const AUTH0_DOMAIN = 'tridnguyen.auth0.com';
+
+let token;
+
+var client = jwksClient({
+  jwksUri: `https://${AUTH0_DOMAIN}/.well-known/jwks.json`
+});
+
+function getKey(header, callback){
+  client.getSigningKey(header.kid, function(err, key) {
+    var signingKey = key.publicKey || key.rsaPublicKey;
+    callback(null, signingKey);
+  });
+}
+
+async function verifyJwt(token) {
+  return new Promise(function (resolve, reject) {
+    jwt.verify(token, getKey, function (err, decoded) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(decoded);
+      }
+    });
+  })
+}
 
 export async function getToken(options) {
   if (!options.clientId || !options.clientSecret) {
     throw new Error('clientId and clientSecret are required');
   }
 
-  const response = await postJson(
-    'https://tridnguyen.auth0.com/oauth/token',
-    {
-      client_id: options.clientId,
-      client_secret: options.clientSecret,
-      audience: options.audience,
-      grant_type: 'client_credentials'
+  if (token) {
+    try {
+      await verifyJwt(token);
+      log('Previous token still valid.');
+      return token;
+    } catch (e) {
+      token = undefined;
     }
-  );
+  }
+  log('Getting client credentials grant access token');
+  const auth0 = new AuthenticationClient({
+    domain: AUTH0_DOMAIN,
+    ...options,
+  });
+  const response = await auth0.clientCredentialsGrant({
+    audience: options.audience,
+  });
+  token = response.access_token;
 
   return response.access_token;
 }
